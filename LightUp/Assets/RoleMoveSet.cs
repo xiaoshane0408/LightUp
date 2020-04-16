d5e361548b742568f31721a035f163c45c03728c
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-04-16 21:28:41 +0800

    新增機器人模型及動作

---
+++ LightUp/Assets/RoleMoveSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleMoveSet : MonoBehaviour
+{
+    //CharacterController role;
+    //Vector3 moveV;
+    //public float g = 8;
+    //// Start is called before the first frame update
+    void Start()
+    {
+        //role = GetComponent<CharacterController>();
+    }
+
+    //void Move()
+    //{
+    //    if(role.isGrounded)
+    //    {
+    //        moveV = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+    //    }
+    //    moveV -= Vector3.down * g * Time.deltaTime;
+    //    role.Move(moveV * Time.deltaTime);
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+     //   Move();
+    }
+}
